561f28dbf7d0c8e0c0586f1b61d6d33a0a4c6478
palm <palmfuruit46@gmail.com>
2023-09-24 12:36:11 +0900

    見た目の修正

--- app/javascript/controllers/auto_modal_controller.js
+++ app/javascript/controllers/auto_modal_controller.js
@@ -4,22 +4,18 @@ import {Controller} from "@hotwired/stimulus"
 export default class extends Controller {
 
     connect() {
-        console.log("auto-modal--connect");
         this.modal = new bootstrap.Modal(this.element);
         this.modal.show();
     }
 
     // 保存成功時にモーダルを閉じる
     close_if_success(event) {
-        console.log("auto-modal--close_if_success");
         if (event.detail.success) {
-            console.log("event-success");
             this.modal.hide();
         }
     }
 
     close(event) {
-        console.log("auto-modal--close");
         this.modal.hide();
     }
 }
