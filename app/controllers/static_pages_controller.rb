4b61ffb9e648d28aa93df90aadbadfb60644c903
palm <palmfuruit46@gmail.com>
2023-10-08 16:21:47 +0900

    TopPage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,12 @@
 class StaticPagesController < ApplicationController
   def home
   end
+
+  def elimination_operation
+  end
+
+  def roundrobin_operation
+  end
   def inquiry
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "static_pages#home"
+  get "elimination_operation" => "static_pages#elimination_operation"
+  get "roundrobin_operation" => "static_pages#roundrobin_operation"
   get "inquiry" => "static_pages#inquiry"
 
   resources :eliminations, except: :index do
