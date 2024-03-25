e12cdc14cbeb28cf2e9d894bf3610f4eb7264317
palm <palmfuruit46@gmail.com>
2024-03-25 16:42:15 +0900

    #26.大会の最終アクセス日を保存

--- db/migrate/20240325023353_add_accessed_on_to_tournament.rb
+++ db/migrate/20240325023353_add_accessed_on_to_tournament.rb
@@ -1,4 +1,4 @@
-class AddLastAccessToTournament < ActiveRecord::Migration[7.0]
+class AddAccessedOnToTournament < ActiveRecord::Migration[7.0]
   def change
     add_column :tournaments, :accessed_on, :date
   end
